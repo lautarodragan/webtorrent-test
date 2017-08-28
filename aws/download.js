8b4831d5d6a640c5fc0ea21b986b8c3cb8efa117
Lautaro Dragan <lautaro.dragan@gmail.com>
2017-08-28 18:29:02 -0300

    more fixes

--- aws/download.js
+++ aws/download.js
@@ -7,8 +7,7 @@ var WebTorrent = require('webtorrent')
 const magnetURI = 'magnet:?xt=urn:btih:d1cbfbfa0bcd7a4f97e6f0a67335ca9c0923777e&dn=20228390_10155298196020325_5318241211675776399_n.jpg&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'
 
 var client2 = new WebTorrent({
-	tracker: false,
-	dht: { bootstrap: '54.211.60.172:20000' } 
+	tracker: false
 })
 
 // setInterval(() => { console.log('dht', client2.dhtPort, client2.dht.toJSON()) }, 1000)
@@ -17,7 +16,12 @@ var client2 = new WebTorrent({
 // client2.dht.on('warning', function (err) { console.error('dht', err) })
 // client2.dht.on('listening', function (err) { console.error('dht listening', client2.dht.address()) })
 // client2.dht.on('query', function (err) { console.error('dht query', err) })
-// client2.dht.on('node', function (err) { console.error('dht node', err) })
+
+client2.dht.on('node', function (node) {
+  if (node.host === '54.211.60.172')
+    console.error('FOUND OUR DHT NODE', node)
+  // console.error('dht node', node)
+})
 
 client2.on('error', function (err) { console.error(err) })
 client2.on('warning', function (err) { console.error(err) })
